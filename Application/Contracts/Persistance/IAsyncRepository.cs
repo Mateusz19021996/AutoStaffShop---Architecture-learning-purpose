99639b1c8390d56bfae49136963af10531c7e672
Mateusz19021996 <mateuszkaleta01@gmail.com>
2021-11-10 00:15:53 +0100

    Add project files.

---
+++ Application/Contracts/Persistance/IAsyncRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.Persistance
+{
+    // T should be a class
+    public interface IAsyncRepository<T> where T: class
+    {
+        Task<T> GetAllAsync();
+
+        Task<IReadOnlyList<T>> GetByIdAsync(Guid id);
+        
+        Task<T> AddAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<T> DeleteAsync(T entity);
+
+        /*  Notki PL :
+         *  
+         *  Klasa Task reprezentuje nam asynchroniczną operację.
+         *  Nie zwraca wartości jednak Task<T> zwraca nam zmienną o typie T
+         *  Jest to swojego rodzaju obietnica, że operacja się zakończy
+         *  
+         *  
+         *
+         * 
+         * 
+         * 
+         */
+    }
+}
